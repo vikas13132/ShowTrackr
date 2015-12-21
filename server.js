705a5b34d38ec2156e84f824f5d6afb6130090b7
vikas13132 <vikas.subramanian@gmail.com>
2015-12-21 17:28:27 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require ('path');
+//var logger = require('logger');
+var cookieparser = require('cookieparser');
+var bodyparser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.port || 3000);
+//app.use(logger('dev'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+//app.use(cookieparser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function () {
+  console.log('Ready');
+})
